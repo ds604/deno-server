26b7b6e09dd14bde8d34c2c2ddb48e442ee8c1fb
David Sarma <davidsarma@gmail.com>
2021-02-20 02:23:11 -0500

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,13 @@
+import {serve} from 'https://deno.land/std@0.61.0/http/server.ts';
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { args } = Deno
+const DEFAULT_PORT = 8000
+const argPort = parse(args).port
+const port = argPort ? Number(argPort) : DEFAULT_PORT
+const s = serve({port: port})
+
+console.log('http://localhost:' + port)
+for await (const req of s){
+	req.respond({ body: 'Hello World\n' })
+}
