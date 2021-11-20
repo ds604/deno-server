3379b62ea70c539f6e26672f772517c7a4055134
David Sarma <davidsarma@gmail.com>
2021-11-19 19:55:52 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,16 +1,25 @@
 import {serve} from 'https://deno.land/std@0.61.0/http/server.ts';
 import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 const { args } = Deno
 const DEFAULT_PORT = 8000
 const argPort = parse(args).port
 const port = argPort ? Number(argPort) : DEFAULT_PORT
-const s = serve({port: port})
 
+const config = <string>Deno.env.get('DATABASE_URL');
+const client = new Client(config);
+
+const s = serve({port: port})
 console.log('http://localhost:' + port)
+
+await client.connect()
 for await (const req of s){
+	const array_result = await client.queryArray("SELECT * FROM Users;")
 	req.respond({ body: `Hello World!\n
 			${new Date()}\n
-			foobar!!!!!
+			foobar!!!!!\n
+			${array_result.rows}
 			`})
 }
+await client.end()
